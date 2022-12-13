f9285a8abb8578aaccf9dfc4c757aabe3c6ce344
Anthony <anthonypenaloza99@gmail.com>
2022-12-13 13:11:13 -0600

    changed port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use('/users', require('./routes/users'))
 
-app.listen(3000, ()=>{
-    console.log("server is up on port 300")
+app.listen(1234, ()=>{
+    console.log("server is up on port 1234")
 })
